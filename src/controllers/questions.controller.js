b0df9752bf8cc78e13a8c3953cca36397873dc28
Vakha Irzakhanov <47324041+irzakhanov@users.noreply.github.com>
2021-11-06 15:59:06 +0300

    Сделал валидацию формы добавлени вопроса (#4)
    * Сделал валидацию формы добавлени вопроса
    
    * Сделал возврата статус кода и поднял приоритет миделвера
    
    Co-authored-by: Vakha <gvardee95@gmail.com>

--- src/controllers/questions.controller.js
+++ src/controllers/questions.controller.js
@@ -1,17 +1,24 @@
+const { validationResult } = require('express-validator');
 const Question = require('../models/Question.model');
 
 module.exports.questionsController = {
   addQuestion: async (req, res) => {
     try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Error', errors });
+      }
+
       const question = await Question.create({
         question: req.body.question,
         comment: req.body.comment,
         user: req.user.userId,
       });
 
-      res.json({ message: 'Question created', question });
+      return res.json({ message: 'Question created', question });
     } catch (e) {
-      res.json({ error: e.toString() });
+      return res.json({ error: e.toString() });
     }
   },
 };
--- src/routes/questions.route.js
+++ src/routes/questions.route.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { questionsController } = require('../controllers/questions.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = Router();
 
-router.post('/', authMiddleware, questionsController.addQuestion);
+router.post(
+  '/',
+  authMiddleware,
+  [
+    check(
+      'question',
+      'Длина текста должна быть больше 4 и меньше 140 символов'
+    ).isLength({ min: 4, max: 140 }),
+  ],
+  questionsController.addQuestion
+);
 
 module.exports = router;
