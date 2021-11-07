d31043a0f27860ba43e885ee961357cef5a632b7
tsugaev <tsuagev@mail.com>
2021-11-07 20:10:29 +0300

    mend

--- src/routes/questions.route.js
+++ src/routes/questions.route.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = Router();
 
-router.get('/', authMiddleware, questionsController.getAllQuestions);
+router.get('/', questionsController.getAllQuestions);
 router.post(
   '/',
   authMiddleware,
