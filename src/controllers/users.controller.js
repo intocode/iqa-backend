f9816043eb992c83a3db80a890f48affd9b725ec
Kuduzow Akhmad <ammya@ya.ru>
2021-12-20 10:57:20 +0300

    Убран аплоад аватарок. Теперь они берутся из gh (#18)

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,26 +1,7 @@
-const fs = require('fs');
-const fsp = require('fs/promises');
-const path = require('path');
-const sharp = require('sharp');
-const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User.model');
 
 const { JWT_SECRET_KEY, JWT_EXPIRES_IN } = process.env;
-
-const downloadImage = (url, dir) =>
-  axios({
-    url,
-    responseType: 'stream',
-  }).then(
-    (response) =>
-      new Promise((resolve, reject) => {
-        response.data
-          .pipe(fs.createWriteStream(dir))
-          .on('finish', () => resolve())
-          .on('error', (e) => reject(e));
-      })
-  );
 
 module.exports.usersController = {
   authUser: async (req, res) => {
@@ -28,31 +9,10 @@ module.exports.usersController = {
       let candidate = await User.findOne({ githubId: req.user.id });
 
       if (!candidate) {
-        try {
-          await fsp.access(path.resolve(process.cwd(), 'static/small'));
-          // eslint-disable-next-line no-console
-          console.log('Папка ./static/small/ существует');
-        } catch (_) {
-          await fsp.mkdir(path.resolve(process.cwd(), 'static/small'), {
-            recursive: true,
-          });
-          // eslint-disable-next-line no-console
-          console.log(
-            `Папка ${path.resolve(process.cwd(), 'static/small')} создана`
-          );
-        }
-
-        const imageName = `${req.user.id}.jpg`;
-
-        await downloadImage(req.user._json.avatar_url, `./static/${imageName}`);
-        await sharp(`./static/${imageName}`)
-          .resize(36, 36)
-          .jpeg()
-          .toFile(`./static/small/${imageName}`);
-
         candidate = await User.create({
           name: req.user.username,
           githubId: req.user.id,
+          avatarUrl: req.user._json.avatar_url,
           email: req.user.emails[0].value,
         });
       }
