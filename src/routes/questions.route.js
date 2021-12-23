79186d49faba2629ba693ac005b162c7a60bd79d
Baisangur <idigov-98@mail.ru>
2021-12-23 15:29:41 +0300

    При получении id отправляется вопрос, иначе весь массив вопросов (#20)

--- src/routes/questions.route.js
+++ src/routes/questions.route.js
@@ -6,7 +6,7 @@ const Question = require('../models/Question.model');
 
 const router = Router();
 
-router.get('/', questionsController.getAllQuestions);
+router.get('/:id?', questionsController.getQuestions);
 router.post(
   '/',
   authMiddleware,
