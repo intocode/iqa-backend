301b020aafb06f996cc81945f57fd73ae911b437
Dukvakha <dukvakha1997@mail.ru>
2022-01-09 00:01:24 +0300

    Удаление вопроса из раздела 'избранные'

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User.model');
 const Question = require('../models/Question.model');
-const { log } = require('nodemon/lib/utils');
 
 const { JWT_SECRET_KEY, JWT_EXPIRES_IN } = process.env;
 
@@ -82,16 +81,16 @@ module.exports.usersController = {
       const question = await Question.findById(req.params.id);
 
       if (!question) {
-        res.status(404).json({
+        return res.status(400).json({
           message: 'Такого вопроса нет',
         });
       }
       const user = await User.findByIdAndUpdate(req.user.userId, {
         $addToSet: { favorites: req.params.id }
       });
-      res.json(user.favorites);
+      return res.json(user.favorites);
     } catch (e) {
-      res.json({
+      return res.json({
         message: `Ошибка при добавлении вопроса в избранные:${e.toString()}`,
       });
     }
