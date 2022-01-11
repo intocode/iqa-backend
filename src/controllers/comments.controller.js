067b3d1d779d95700e098e5cef30f5e58c03f0e5
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2022-01-11 14:17:46 +0300

    Поправил populate чтобы показывал аватар

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -29,7 +29,7 @@ module.exports.commentsController = {
 
       const comments = await Comment.find({
         questionId: id,
-      }).populate('author', { _id: 1, name: 1, avatar: 1 });
+      }).populate('author', { _id: 1, name: 1, avatarUrl: 1 });
 
       return res.json(comments);
     } catch (e) {
