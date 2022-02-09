71cdd4edc5aedcaef2798fb25f0e8c004636869e
Ali <alitm.s@mail.ru>
2022-02-09 18:42:41 +0300

    Изменил логику при удалении и выводе вопросов (#38)
    * добавил ключ deleted в модель вопроса
    
    * изменил логику контроллера при удалении вопроса
    
    * изменил вывод вопросов для админа и пользователей
    
    * добавил роутер и контроллер для восст-я вопросов
    
    * исправил баги, применил prettier
    
    * исправил баг контроллера восстановления

--- src/controllers/questions.controller.js
+++ src/controllers/questions.controller.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 const Question = require('../models/Question.model');
 const User = require('../models/User.model');
 
@@ -39,7 +40,27 @@ module.exports.questionsController = {
         return res.json(question);
       }
 
-      const allQuestions = await Question.find()
+      if (req.headers.authorization) {
+        const { authorization } = req.headers;
+        const [type, token] = authorization.split(' ');
+
+        if (type !== 'Bearer') {
+          throw new Error("wrong token's type");
+        }
+
+        const authUser = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await User.findById(authUser.userId);
+
+        if (user.isAdmin) {
+          const allQuestions = await Question.find()
+            .populate('tags', { _id: 0, name: 1, color: 1 })
+            .populate('user', { name: 1, githubId: 1, avatarUrl: 1 }); // fix avatarUrl: 1
+
+          return res.json(allQuestions);
+        }
+      }
+
+      const allQuestions = await Question.find({ deleted: { $ne: true } })
         .populate('tags', { _id: 0, name: 1, color: 1 })
         .populate('user', { name: 1, githubId: 1, avatarUrl: 1 }); // fix avatarUrl: 1
 
@@ -56,10 +77,28 @@ module.exports.questionsController = {
       const user = await User.findById(userId);
 
       if (user.isAdmin) {
-        await Question.findByIdAndRemove(id);
-        const allQuestions = await Question.find();
+        await Question.findByIdAndUpdate(id, { $set: { deleted: true } });
 
-        return res.json(allQuestions);
+        return res.json({ message: 'Question deleted' });
+      }
+
+      return res.json({ error: 'У вас недостаточно прав' });
+    } catch (e) {
+      return res.status(400).json({ error: e.toString() });
+    }
+  },
+
+  restoreQuestion: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { userId } = req.user;
+
+      const user = await User.findById(userId);
+
+      if (user.isAdmin) {
+        await Question.findByIdAndUpdate(id, { $set: { deleted: 'false' } });
+
+        return res.json({ message: 'Question restored' });
       }
 
       return res.json({ error: 'У вас недостаточно прав' });
