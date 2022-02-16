8926c668f95b4705560c90f5b19ed1f1011f7ef8
Ali <alitm.s@mail.ru>
2022-02-16 15:01:48 +0300

    исправил вывод вопросов для пользователей (#41)
    * исправил вывод вопросов для пользователей
    
    * исправил  try...catch

--- src/controllers/questions.controller.js
+++ src/controllers/questions.controller.js
@@ -50,17 +50,20 @@ module.exports.questionsController = {
       if (req.headers.authorization) {
         const { authorization } = req.headers;
         const [, token] = authorization.split(' ');
+        /* eslint no-empty: ["error", { "allowEmptyCatch": true }] */
+        try {
+          const authUser = await jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-        const authUser = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-        const user = await User.findById(authUser.userId);
+          const user = await User.findById(authUser.userId);
 
-        if (user.isAdmin) {
-          const allQuestions = await Question.find()
-            .populate('tags', { _id: 0, name: 1, color: 1 })
-            .populate('user', { name: 1, githubId: 1, avatarUrl: 1 }); // fix avatarUrl: 1
+          if (user.isAdmin) {
+            const allQuestionsForAdmin = await Question.find()
+              .populate('tags', { _id: 0, name: 1, color: 1 })
+              .populate('user', { name: 1, githubId: 1, avatarUrl: 1 }); // fix avatarUrl: 1
 
-          return res.json(allQuestions);
-        }
+            return res.json(allQuestionsForAdmin);
+          }
+        } catch (e) {}
       }
 
       const allQuestions = await Question.find({ deleted: { $ne: true } })
