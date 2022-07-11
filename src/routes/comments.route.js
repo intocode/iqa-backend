58263b2d3fea63ed98524f3ddd7f6f3a963ba962
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-07-11 11:24:36 +0300

    Реализовал эндпоинт для удаления комментария

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const {
   getCommentsByQuestionIdController,
   addNewCommentController,
+  deleteCommentController,
 } = require('../controllers/comments.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
@@ -9,5 +10,6 @@ const router = Router({ mergeParams: true });
 
 router.get('/', getCommentsByQuestionIdController);
 router.post('/', authMiddleware, addNewCommentController);
+router.delete('/:id', authMiddleware, deleteCommentController);
 
 module.exports = router;
