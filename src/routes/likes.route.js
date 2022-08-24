bef1af551ccdaaef6e1730d11bfbe2e3abdb300c
mxns <mxnsur.hh@gmail.com>
2022-08-24 18:15:28 +0300

    Добавил роуты для лайков

--- src/routes/likes.route.js
+++ src/routes/likes.route.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
-const {
-  addNewLikeController,
-  deleteLikeController,
-} = require('../controllers/likes.controller');
+const { addLikeController, removeLikeController } = require('../controllers/likes.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = Router({ mergeParams: true });
 
-router.post('/', authMiddleware, addNewLikeController);
-router.delete('/:id', authMiddleware, deleteLikeController);
+router.post('/:id', authMiddleware, addLikeController);
+router.delete('/:id', authMiddleware, removeLikeController);
 
 module.exports = router;
