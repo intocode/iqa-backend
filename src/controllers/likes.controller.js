5b1c06fb561289f1a8cf1480b0427b7ed73a8381
mxns <mxnsur.hh@gmail.com>
2022-08-25 20:14:47 +0300

    Убрал бесполезные проверки и исправил ошибки

--- src/controllers/likes.controller.js
+++ src/controllers/likes.controller.js
@@ -1,28 +1,18 @@
-const Comment = require('../models/Comment.model');
-const { addLike, removeLikeById } = require('../services/likes.service');
-const ApiError = require('../utils/ApiError.class');
+const { likedComment, unlikedComment } = require('../services/likes.service');
+const { getComments } = require('../services/comments.service');
 const { catchError } = require('../utils/catchError');
 
 const addLikeController = catchError(async (req, res) => {
   // ID комментария, где ставится лайк
   const { id } = req.params;
   // ID пользователя, который ставит лайк
-  const { userId } = req.body;
+  const { userId } = req.user;
 
-  const liked = await Comment.findOne({ _id: id, likes: userId });
+  const addALike = await likedComment({ id, userId });
 
-  if (!liked) {
-    throw new ApiError('Вы уже лайкнули данный комментарий.', 400);
-  }
-
-  const addALike = await addLike({ id, likes: req.user.userId });
-
-  const populatedComment = await Comment.findById(addALike._id).populate('author', {
-    id: 1,
-    name: 1,
-    avatarUrl: 1,
-  });
-
+  const populatedComment = await getComments({ _id: addALike._id });
+  
+  // возвращаем объект комментария, чтоб было легче обновить его состояние на фронте
   return res.json(populatedComment);
 });
 
@@ -33,17 +23,12 @@ const removeLikeController = catchError(async (req, res) => {
   // ID текущего юзера
   const { userId } = req.user;
 
-  // Нужно проверить есть права у текущего пользователя
-  const liked = await Comment.findOne({ _id: id, likes: userId });
+  const unLiked = await unlikedComment({ id, userId });
 
-  if (liked) {
-    await removeLikeById(id);
-
-    return res.json({ message: 'Лайк удалён' });
-  }
-
-  // если не удалось удалить
-  throw new ApiError('У вас недостаточно прав для удаления лайка', 403);
+  const populatedComment = getComments({ _id: unLiked._id });
+  
+  // возвращаем объект комментария, чтоб было легче обновить его состояние на фронте
+  return res.json(populatedComment);
 });
 
 module.exports = {
