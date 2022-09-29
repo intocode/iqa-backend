09a19b8012140aa897910843a464e26e236eb73c
Adam <98719246+baysarov077@users.noreply.github.com>
2022-09-29 20:53:28 +0300

    Добавил возможность изменить имя и почту (#92)
    * Добавил возможность изменить имя и почту
    
    * Изменил контроллер для изменения имени и почты
    
    * Изменил контроллер updateProfile
    
    * Провел рефакторинг кода

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -90,4 +90,24 @@ module.exports.usersController = {
   userCheck: async (req, res) => {
     res.json(`hello ${req.user.name}`);
   },
+
+  updateProfile: async (req, res) => {
+    try {
+      const acceptFields = ['fullName', 'email'];
+      const updateObject = Object.keys(req.body).reduce((accum, key) => {
+        if (acceptFields.includes(key)) {
+          if (req.body[key]) {
+            // eslint-disable-next-line no-param-reassign
+            accum[key] = req.body[key];
+          }
+        }
+        return accum;
+      }, {});
+
+      const userPatch = await User.findByIdAndUpdate(req.params.id, updateObject, { new: true });
+      res.json(userPatch);
+    } catch (e) {
+      res.status(400).json({ error: e.toString() });
+    }
+  },
 };
